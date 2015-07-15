279e9a3cc6f641cab8aae52568df8048dc0aa631
coenandriessen <coen.andriessen@student.hu.nl>
2015-07-15 21:20:02 +0200

    Added doxygen and comments

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,39 +1,58 @@
 #include "StudentPreProcessing.h"
 #include "basetimer.h"
 
+/**
+* Functie voor het converteren van een afbeedling van rgb naar grijswaarde.
+*
+* @param const	RGBImage &image
+*
+* @return		output
+*/
+
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
+	// Maak een basetimer aan. De basetimer wordt gebruikt om de tijd bij te houden
+	// die de implementatie gebruikt.
 	BaseTimer basetimer;
+	// Start de basetimer.
 	basetimer.start();
+	// Maak de output voor de afeelding aan.
 	IntensityImageStudent * output = new IntensityImageStudent();
+	// Set de hoogte en breedte van de output.
 	output->set(image.getWidth(), image.getHeight());
-	for (int i = 0; i < image.getWidth(); i++){
-		for (int ii = 0; ii < image.getHeight(); ii++){
+	// Loop elke pixel van de afbeelding door.
+	for (int i = 0; i < image.getWidth(); i++)
+	{
+		for (int ii = 0; ii < image.getHeight(); ii++)
+		{
+			// Maak pixel object aan.
 			Intensity pixel;
+			// Maak rgb object aan met de rgb waarde van de orginele afbeelding.
 			RGB rgb = image.getPixel(i, ii);
-			// Luma / Luminance
+			// Luma / Luminance formule voor omzetten van rbg naar grijswaarde.
 			pixel = ((0.3f * rgb.r) + (0.59f*  rgb.g) + (0.11 * rgb.b));
-
-			// Averaging
+			// Averaging formule voor omzetten van rbg naar grijswaarde.
 			//pixel = ((rgb.r + rgb.g + rgb.b) / 3);
-
-			// Luster
+			// Luster formule voor omzetten van rbg naar grijswaarde.
 			//pixel = ((std::min({ rgb.b, rgb.g, rgb.r })) + (std::max({ rgb.b, rgb.g, rgb.r })) / 2);
-
+			// Set de outputpixel aan de nieuwe waarde.
 			output->setPixel(i, ii, pixel);
 		}
 	}
+	// Stop de timer
 	basetimer.stop();
+	// Schrijf de tijd dat nodig is geweest naar een output file.
 	std::ofstream myfile;
 	myfile.open("tijd.txt", std::ofstream::ate);
-	myfile << "Luma convert tijd in s: " << basetimer.elapsedSeconds() << " tijd ms:" << basetimer.elapsedMilliSeconds() << " tijd us" << basetimer.elapsedMicroSeconds();
+	myfile << "Luma convert tijd in s: " << basetimer.elapsedSeconds() << " tijd ms:" 
+		<< basetimer.elapsedMilliSeconds() << " tijd us" << basetimer.elapsedMicroSeconds();
 	myfile.close();
 	return output;
-	
 }
-IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
 
+IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
 	return stepScaleImageFirstOrder(image);
 }
+
 IntensityImage * StudentPreProcessing::stepScaleImageZeroOrder(const IntensityImage &image) const {
 	IntensityImage* product = new IntensityImageStudent(200, 200);
 	float yScale = image.getHeight() / 200.0f;
@@ -45,11 +64,8 @@ IntensityImage * StudentPreProcessing::stepScaleImageZeroOrder(const IntensityIm
 			//std::cout << xScale* Xcord << " " << yScale* Ycord << "\n";
 			Intensity pixel = image.getPixel(std::round(xScale* Xcord), std::round(yScale* Ycord));
 			product->setPixel(Xcord, Ycord, pixel);
-
-
 		}
 	}
-
 	std::cout << yScale << " " << xScale << "\n";
 	return product;
 }
@@ -78,13 +94,9 @@ IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &im
 		}
 	}
 	return product;
-
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImageFirstOrder(const IntensityImage &image) const {
-
-
-
 	IntensityImage* product = new IntensityImageStudent(200, 200);
 	float yScale = image.getHeight() / 200.0f;
 	float xScale = image.getWidth() / 200.0f;
@@ -97,17 +109,10 @@ IntensityImage * StudentPreProcessing::stepScaleImageFirstOrder(const IntensityI
 			Intensity pixel2 = image.getPixel(std::floor(xScale* Xcord), std::ceil(yScale* Ycord));
 			Intensity pixel3 = image.getPixel(std::ceil(xScale* Xcord), std::floor(yScale* Ycord));
 			Intensity pixel4 = image.getPixel(std::ceil(xScale* Xcord), std::ceil(yScale* Ycord));
-
-
-
 			Intensity pixel = (pixel1 + pixel2 + pixel3 + pixel4) / 4;
 			product->setPixel(Xcord, Ycord, pixel);
-
-
 		}
 	}
-
 	std::cout << yScale << " " << xScale << "\n";
-
 	return product;
 }
